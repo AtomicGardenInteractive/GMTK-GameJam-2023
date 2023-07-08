e419a21854a8710d2a5b85aa343c14dfd96aa50d
Arnas Puskunigis <arnaspusk204@gmail.com>
2023-07-08 03:43:04 +0100

    Main menu completed, not visuals just blank background and text

---
+++ GMTK 2023/Assets/Scripts/MainMenuUiController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUiController : MonoBehaviour
+{
+
+    [SerializeField] Button PlayBtn;
+    [SerializeField] Button OptionsBtn;
+    [SerializeField] Button CloseOptionsBtn;
+    [SerializeField] Button ExitBtn;
+
+    [SerializeField] GameObject OptionsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayBtn.onClick.AddListener(PlayPressed);
+        OptionsBtn.onClick.AddListener(OptionsPressed);
+        CloseOptionsBtn.onClick.AddListener(CloseOptionsPressed);
+        ExitBtn.onClick.AddListener(ExitPressed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void PlayPressed()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    private void OptionsPressed()
+    {
+        OptionsMenu.SetActive(true);
+    }
+
+    private void CloseOptionsPressed()
+    {
+        OptionsMenu.SetActive(false);
+    }
+
+    private void ExitPressed()
+    {
+        Application.Quit();
+    }
+
+}
